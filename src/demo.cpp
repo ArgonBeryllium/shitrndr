c7f4c7233be70430127bc606a0b48af6eaf15beb
ArBe <argon-beryllium@protonmail.com>
2020-12-04 21:28:57 +0100

    some demo changes

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,5 +1,8 @@
 #include "shitrndr.h"
 
+static constexpr uint16_t CELL_SIZE = 32;
+static uint16_t cw = 16, ch = 16;
+
 #if defined _WIN32 || defined __CYGWIN__
 int WinMain()
 #else
@@ -16,25 +19,32 @@ int main()
 		bg_col.r = Input::getMB(1)?15:215;
 		int mx = (int)Input::getMP().x, my = (int)Input::getMP().y;
 
-		constexpr int SIZE = 32;
-		constexpr int COUNT = 512/SIZE;
-		for(int y = 0; y != COUNT; y++)
-			for(int x = 0; x != COUNT; x++)
+		constexpr int SIZE = 32; 
+		for(int y = 0; y != ch; y++)
+			for(int x = 0; x != cw; x++)
 			{
-				SDL_SetRenderDrawColor(ren, x * 255/COUNT, y * 255/COUNT, 0, 255);
+				SDL_SetRenderDrawColor(ren, x * 255/cw, y * 255/ch, 0, 255);
 				int s = SIZE * (.6 + std::sin(t*4 + (x + y)/2) / 3);
 				
-				int dst = (mx-20-SIZE*x)*(mx-20-SIZE*x)+(my-20-SIZE*y)*(my-20-SIZE*y);
-				dst /= SIZE;
-				if(dst < 4*SIZE)
+				int dst = (mx-20-CELL_SIZE*x)*(mx-20-CELL_SIZE*x)+(my-20-CELL_SIZE*y)*(my-20-CELL_SIZE*y);
+				dst /= CELL_SIZE;
+				if(dst < 4*CELL_SIZE)
 				{
 					s /= 5;
 					s += d/5;
 				}
-				FillRect({(int)(20 + SIZE*x), (int)(20 + SIZE*y), s, s});
+				FillRect({(int)(20 + CELL_SIZE*x), (int)(20 + CELL_SIZE*y), s, s});
 			}
 
-		SDL_Delay(16); // to prevent CPU overloading
+		SDL_Delay(16);
+	};
+	onEvent = [](SDL_Event* event)
+	{
+		if(event->window.event==SDL_WINDOWEVENT_RESIZED)
+		{
+			cw = (event->window.data1-35)/CELL_SIZE;
+			ch = (event->window.data2-35)/CELL_SIZE;
+		}
 	};
 	onKeyDown = [](const SDL_Keycode& key) { std::cout << SDL_GetKeyName(key) << " pressed.\n"; };
 
