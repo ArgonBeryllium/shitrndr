cea5dd4d190c4813e277b4db39983eb0a4562033
ArBe <argon-beryllium@protonmail.com>
2022-08-30 12:52:09 +0200

    introduced convention of an underscore prefix for internal-use functions

--- src/shitrndr.h
+++ src/shitrndr.h
@@ -116,11 +116,11 @@ inline SDL_Colour bg_col = {0xCC, 0xCC, 0xCC, 0xFF};
 inline static SDL_Colour bars_col = {0,0,0,255};
 
 // DEFAULT EVENT VOIDS //
-inline void defOnKey(const SDL_Keycode& key) { std::cout << "key action: " << SDL_GetKeyName(key) << '\n'; }
-inline void defOnMB(const uint8_t& but) { std::cout << "mouse button action: " << but << '\n'; }
-inline void defOnMM(const helpers::vec2<int>& mp) { std::cout << "mouse moved: { " << mp.x << ", " << mp.y << " }\n"; }
-inline void defOnEvent(SDL_Event* event) { std::cout << "SDL event: " << event->type << '\n'; }
-inline void defOnRender(double d, double t)
+inline void _defOnKey(const SDL_Keycode& key) { std::cout << "key action: " << SDL_GetKeyName(key) << '\n'; }
+inline void _defOnMB(const uint8_t& but) { std::cout << "mouse button action: " << but << '\n'; }
+inline void _defOnMM(const helpers::vec2<int>& mp) { std::cout << "mouse moved: { " << mp.x << ", " << mp.y << " }\n"; }
+inline void _defOnEvent(SDL_Event* event) { std::cout << "SDL event: " << event->type << '\n'; }
+inline void _defOnRender(double d, double t)
 {
 	std::cout << "delta: " << d << '\n';
 	std::cout << "elapsed: " << t << '\n';
@@ -128,14 +128,14 @@ inline void defOnRender(double d, double t)
 /////////////////////////
 
 // EVENT FUNCTION POINTERS //
-inline std::function<void(const SDL_Keycode& key)> onKeyDown = &defOnKey;
-inline std::function<void(const SDL_Keycode& key)> onKeyHeld = &defOnKey;
-inline std::function<void(const SDL_Keycode& key)> onKeyUp = &defOnKey;
-inline std::function<void(const uint8_t& but)> onMBDown = &defOnMB;
-inline std::function<void(const uint8_t& but)> onMBUp = &defOnMB;
-inline std::function<void(const helpers::vec2<int>& mp)> onMouseMoved = &defOnMM;
-inline std::function<void(SDL_Event* event)> onEvent = &defOnEvent;
-inline std::function<void(double delta, double time)> onRender = &defOnRender;
+inline std::function<void(const SDL_Keycode& key)> onKeyDown = &_defOnKey;
+inline std::function<void(const SDL_Keycode& key)> onKeyHeld = &_defOnKey;
+inline std::function<void(const SDL_Keycode& key)> onKeyUp = &_defOnKey;
+inline std::function<void(const uint8_t& but)> onMBDown = &_defOnMB;
+inline std::function<void(const uint8_t& but)> onMBUp = &_defOnMB;
+inline std::function<void(const helpers::vec2<int>& mp)> onMouseMoved = &_defOnMM;
+inline std::function<void(SDL_Event* event)> onEvent = &_defOnEvent;
+inline std::function<void(double delta, double time)> onRender = &_defOnRender;
 /////////////////////////////
 
 struct WindowProps
@@ -151,7 +151,7 @@ public:
 	inline static SDL_Texture* renProxy;
 	inline static Uint32 format;
 
-	static void updateSize()
+	static void _updateSize()
 	{
 		SDL_SetWindowSize(win, w, h);
 
@@ -168,7 +168,7 @@ public:
 #endif
 		}
 	}
-	static void updateRenProxy()
+	static void _updateRenProxy()
 	{
 #if defined _WIN32 || defined __CYGWIN__ || defined __EMSCRIPTEN__
 		SDL_Rect r = getSizeRect();
@@ -187,15 +187,15 @@ public:
 		SDL_UpdateTexture(WindowProps::renProxy, 0, pixels, pitch);
 #endif
 	}
-	static void setSize(const int& w_, const int& h_) { w = w_; h = h_; if(!getLocked()) { sw = w/pixScale; sh = h/pixScale; } updateSize(); }
-	static void setSize(const helpers::vec2<int>& s) { setSize(s.x, s.y); }
-	static void setWidth(const int& w_) { w = w_; updateSize(); }
-	static void setHeight(const int& h_) { h = h_; updateSize(); }
-	static void init(const int& w_, const int& h_)
+	static void _setSize(const int& w_, const int& h_) { w = w_; h = h_; if(!getLocked()) { sw = w/pixScale; sh = h/pixScale; } _updateSize(); }
+	static void _setSize(const helpers::vec2<int>& s) { _setSize(s.x, s.y); }
+	static void _setWidth(const int& w_) { w = w_; _updateSize(); }
+	static void _setHeight(const int& h_) { h = h_; _updateSize(); }
+	static void _init(const int& w_, const int& h_)
 	{
 		sw = w = w_;
 		sh = h = h_;
-		updateSize();
+		_updateSize();
 	}
 	//////////////////////
 
@@ -215,8 +215,8 @@ public:
 	static inline bool getLocked() { return slocked; }
 	static inline lockType getLockType() { return lock_type; }
 
-	static void setScaledSize(const int& sw_, const int& sh_) { if(getLocked()) return; sw = sw_; sh = sh_; pixScale = (float)w/(float)sw; updateSize(); }
-	static void setPixScale(const int& scale) { if(getLocked()) return; pixScale = scale; sw = w/pixScale; sh = h/pixScale; updateSize(); }
+	static void setScaledSize(const int& sw_, const int& sh_) { if(getLocked()) return; sw = sw_; sh = sh_; pixScale = (float)w/(float)sw; _updateSize(); }
+	static void setPixScale(const int& scale) { if(getLocked()) return; pixScale = scale; sw = w/pixScale; sh = h/pixScale; _updateSize(); }
 	static void setLocked(const bool& lock) { slocked = lock; }
 	static void setLockType(lockType type) { lock_type = type; }
 	//////////////////////
@@ -228,7 +228,7 @@ private:
 	inline static std::map<uint8_t, bool> mbs;
 	inline static helpers::vec2<int> m_pos;
 public:
-	static void init()
+	static void _init()
 	{
 		m_pos = {};
 		keys = {};
@@ -236,19 +236,19 @@ public:
 	}
 
 	// For internal use //
-	static void setKey(const SDL_Keycode& key, const bool& state)
+	static void _setKey(const SDL_Keycode& key, const bool& state)
 	{
 		keys[key] = state;
 		if(state)	onKeyDown(key);
 		else		onKeyUp(key);
 	}
-	static void setMB(const uint8_t& but, const bool& state)
+	static void _setMB(const uint8_t& but, const bool& state)
 	{
 		mbs[but] = state;
 		if(state)	onMBDown(but);
 		else		onMBUp(but);
 	}
-	static void setMP(const int& x, const int& y) { m_pos.x = x; m_pos.y = y; onMouseMoved(m_pos); }
+	static void _setMP(const int& x, const int& y) { m_pos.x = x; m_pos.y = y; onMouseMoved(m_pos); }
 	//////////////////////
 
 	// For external use //
@@ -269,8 +269,8 @@ inline void init(const char* name, int w, int h, bool resizable = 1, int winFlag
 	win = SDL_CreateWindow(name, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, (resizable ? SDL_WINDOW_RESIZABLE : 0) | winFlags);
 	ren = SDL_CreateRenderer(win, -1, renFlags | SDL_RENDERER_TARGETTEXTURE);
 
-	Input::init();
-	WindowProps::init(w, h);
+	Input::_init();
+	WindowProps::_init(w, h);
 }
 
 // For internal use only //
@@ -279,9 +279,9 @@ inline SDL_Event ev;
 inline Uint32 last = SDL_GetTicks();
 inline double delta, elapsed = 0;
 
-inline void loopCycle()
+inline void _loopCycle()
 #else
-inline void loopCycle(SDL_Event& ev, Uint32& last, double& delta, double& elapsed)
+inline void _loopCycle(SDL_Event& ev, Uint32& last, double& delta, double& elapsed)
 #endif
 {
 	while(SDL_PollEvent(&ev) && ev.window.event != SDL_WINDOWEVENT_CLOSE)
@@ -292,17 +292,17 @@ inline void loopCycle(SDL_Event& ev, Uint32& last, double& delta, double& elapse
 		case SDL_MOUSEMOTION:
 			int mx, my;
 			SDL_GetMouseState(&mx, &my);
-			Input::setMP(mx, my);
+			Input::_setMP(mx, my);
 			break;
 		case SDL_KEYDOWN:
 			if(Input::getKey(ev.key.keysym.sym)) onKeyHeld(ev.key.keysym.sym);
-			else Input::setKey(ev.key.keysym.sym, 1); break;
-		case SDL_KEYUP:		Input::setKey(ev.key.keysym.sym, 0); break;
-		case SDL_MOUSEBUTTONDOWN:	 Input::setMB(ev.button.button, 1); break;
-		case SDL_MOUSEBUTTONUP:		 Input::setMB(ev.button.button, 0); break;
+			else Input::_setKey(ev.key.keysym.sym, 1); break;
+		case SDL_KEYUP:		Input::_setKey(ev.key.keysym.sym, 0); break;
+		case SDL_MOUSEBUTTONDOWN:	 Input::_setMB(ev.button.button, 1); break;
+		case SDL_MOUSEBUTTONUP:		 Input::_setMB(ev.button.button, 0); break;
 		}
 		if(ev.window.event==SDL_WINDOWEVENT_RESIZED)
-			WindowProps::setSize(ev.window.data1, ev.window.data2); 
+			WindowProps::_setSize(ev.window.data1, ev.window.data2); 
 	}
 #ifdef __EMSCRIPTEN__
 	if(ev.window.event==SDL_WINDOWEVENT_CLOSE)
@@ -323,7 +323,7 @@ inline void loopCycle(SDL_Event& ev, Uint32& last, double& delta, double& elapse
 		SDL_Rect r = WindowProps::getSizeRect();
 		SDL_Rect d;
 
-		WindowProps::updateRenProxy();
+		WindowProps::_updateRenProxy();
 
 		if(WindowProps::getLocked())
 		{
@@ -384,11 +384,11 @@ inline void loop()
 	double delta, elapsed = 0;
 
 	while(ev.window.event != SDL_WINDOWEVENT_CLOSE)
-		loopCycle(ev, last, delta, elapsed);
+		_loopCycle(ev, last, delta, elapsed);
 
 	SDL_Quit();
 #else
-	emscripten_set_main_loop(loopCycle, 0, 1);
+	emscripten_set_main_loop(_loopCycle, 0, 1);
 #endif
 }
 
